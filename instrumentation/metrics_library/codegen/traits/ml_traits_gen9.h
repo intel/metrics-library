703c3375f6bfce4e3f43bc2118676a421b90fb4b
Aleksei Keisel <aleksei.keisel@intel.com>
2021-08-16 08:36:45 +0200

    Updated Metrics Library license headers (#11)

--- instrumentation/metrics_library/codegen/traits/ml_traits_gen9.h
+++ instrumentation/metrics_library/codegen/traits/ml_traits_gen9.h
@@ -1,25 +1,12 @@
-/******************************************************************************\
+/*========================== begin_copyright_notice ============================
 
-Copyright © 2021, Intel Corporation
+Copyright © 2020-2021 Intel Corporation
 
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
+SPDX-License-Identifier: MIT
 
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
+============================= end_copyright_notice ===========================*/
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-
+/*
 @file ml_traits_gen9.h
 
 @brief Auto-generated file. Creates Traits structures for a given gen / os / api.
@@ -29,8 +16,7 @@ DO NOT EDIT
 Script file:        Tools/MetricsLibraryGenerator/generators/generate_umd.py
 Script function:    generate_traits
 Template:           Tools/MetricsLibraryGenerator/templates/traits.h
-
-\******************************************************************************/
+*/
 
 #pragma once
 
